90a650afa186aa1559e2c2d8a503c3b226f187f9
KihaJeong1994 <jkha7371@gmail.com>
2024-12-17 23:28:13 +0900

    EnableBatchProcessing - spring batch 실행을 위한 빈 설정, 초기화. H2 Datasource 추가

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,9 +1,12 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // 설정 클래스가 실행시키며, spring batch의 모든 초기화 및 실행 구성.
+// bean 으로 등록된 모든 job 검색, 초기화, 수행
 public class SpringBatchApplication {
 
     public static void main(String[] args) {
