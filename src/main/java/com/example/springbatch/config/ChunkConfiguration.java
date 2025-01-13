2ac37a8f3e265f06a95c36052d5696090f5b3398
james.aims <james.aims@kakaoinsurecorp.com>
2025-01-13 22:01:04 +0900

    ItemProcessor에서 null return 시 ItemWriter에서 자동 제외

--- src/main/java/com/example/springbatch/config/ChunkConfiguration.java
+++ src/main/java/com/example/springbatch/config/ChunkConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +23,7 @@ public class ChunkConfiguration {
     public Job chunkConfigurationJob() {
         return jobBuilderFactory.get("chunkConfigurationJob")
                 .start(chunConfigurationkStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
@@ -30,7 +32,12 @@ public class ChunkConfiguration {
         return stepBuilderFactory.get("chunConfigurationkStep")
                 .<String, String>chunk(5)
                 .reader(new ListItemReader<>(Arrays.asList("item1","item2","item3","item4","item5","item6","item7","item8","item9","item10")))
-                .processor((ItemProcessor<String, String>) item -> "processed" + item)
+                .processor((ItemProcessor<String, String>) item -> {
+                    if(item.equals("item1")) {
+                        return null; // Processor에서 null return 시 writer에서 사용하는 Chunk<O> 에서 자동 제외
+                    }
+                    return "processed" + item;
+                })
                 .writer(items -> {
                     System.out.println("items: " + items);
                 })
