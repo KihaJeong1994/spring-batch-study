a41e5f286f4c585f1a6ef9777459bf436986b59a
james.aims <james.aims@kakaoinsurecorp.com>
2025-01-18 16:15:37 +0900

    JpaCursorItemReader 활용

--- src/main/java/com/example/springbatch/config/JpaCursorConfiguration.java
+++ src/main/java/com/example/springbatch/config/JpaCursorConfiguration.java
@@ -9,34 +9,37 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Configuration
-public class JdbcCursorConfiguration {
+public class JpaCursorConfiguration {
 
     private final int chunkSize = 10;
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final DataSource dataSource;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Bean
-    public Job jdbcCursorJob() {
-        return jobBuilderFactory.get("jdbcCursorJob")
-                .start(jdbcCursorStep())
+    public Job jpaCursorJob() {
+        return jobBuilderFactory.get("jpaCursorJob")
+                .start(jpaCursorStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Step jdbcCursorStep() {
-        return stepBuilderFactory.get("jdbcCursorStep")
+    public Step jpaCursorStep() {
+        return stepBuilderFactory.get("jpaCursorStep")
                 .<Customer, Customer>chunk(chunkSize)
-                .reader(jdbcCursorItemReader(dataSource))
+                .reader(jpaCursorItemReader())
                 .writer(items -> {
                     System.out.println("items: " + items);
                 })
@@ -45,14 +48,12 @@ public class JdbcCursorConfiguration {
     }
 
     @Bean
-    public ItemReader<Customer> jdbcCursorItemReader(DataSource dataSource) {
-        return new JdbcCursorItemReaderBuilder<Customer>()
-                .name("jdbcCursorItemReader")
-                .fetchSize(chunkSize)
-                .sql("select id, firstName, lastName, birthDate from customer where firstName like ? order by lastName, firstName")
-                .beanRowMapper(Customer.class)
-                .queryArguments("A%")
-                .dataSource(dataSource)
+    public ItemReader<Customer> jpaCursorItemReader() {
+        return new JpaCursorItemReaderBuilder<Customer>()
+                .name("jpaCursorItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select c from Customer c where firstName like :firstName order by lastName, firstName")
+                .parameterValues(Map.of("firstName","A%"))
                 .build();
     }
 }
--- src/main/java/com/example/springbatch/data/Customer.java
+++ src/main/java/com/example/springbatch/data/Customer.java
@@ -2,8 +2,15 @@ package com.example.springbatch.data;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
     private Long id;
     private String firstName;
     private String lastName;
